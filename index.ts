524011dd17f53d601c8ce8a265259f855e47ef20
fanttasy <470618507@qq.com>
2023-02-24 22:32:18 +0800

    feat: message sending and receiving

--- index.ts
+++ index.ts
@@ -1,12 +1,32 @@
 import express, { Express, Request, Response } from 'express'
+import path from 'path'
+import http from 'http'
+import { Server, type Socket } from 'socket.io'
 
 const app: Express = express()
-const port = 8080
+const server = http.createServer(app)
+const io = new Server(server)
+const port = 3002
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello express')
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket: Socket) => {
+  socket.emit('message', 'Welcome to ChatRoom')
+
+  // 当有人进入
+  socket.broadcast.emit('message', 'A user has joined the ChatRoom')
+
+  // 当有人退出
+  socket.on('disconnect', () => {
+    io.emit('message', 'A use has left the ChatRoom')
+  })
+
+  // 监听消息
+  socket.on('chatMessage', message => {
+    io.emit('message', message)
+  })
 })
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`)
 })

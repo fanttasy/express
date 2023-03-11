c4bc45927ade55dc387ecf55452b40afdb216667
fanttasy <470618507@qq.com>
2023-03-11 22:18:00 +0800

    feat: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+const { resolve } = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './index.ts',
   output: {
-    filename: "bundle.js"
+    filename: "bundle.js",
+    path: resolve(__dirname, 'dist'),
+    clean: true
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
